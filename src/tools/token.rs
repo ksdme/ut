fbfaf04f253f8d3f519efdcbc75c6c104f48ce34
Kilari Teja <kilariteja9@gmail.com>
2025-09-19 03:13:31 +0530

    chore: cargo fmt

--- src/tools/token.rs
+++ src/tools/token.rs
@@ -4,7 +4,10 @@ use clap::{Command, CommandFactory, Parser};
 use rand::{Rng, rngs::OsRng};
 
 #[derive(Parser, Debug)]
-#[command(name = "token", about = "Generate a cryptographically secure random token.")]
+#[command(
+    name = "token",
+    about = "Generate a cryptographically secure random token."
+)]
 pub struct Token {
     /// Length of the token to generate
     #[arg(long, short, default_value = "64")]
